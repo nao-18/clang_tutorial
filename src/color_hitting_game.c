3b52562d40e85eb5ad26552c51b5dfee3310e8e9
nao18 <kyahyo17@yahoo.co.jp>
2019-12-31 17:02:43 +0900

    [modify]コンソールで文字列受け取り表示する処理実装

--- src/color_hitting_game.c
+++ src/color_hitting_game.c
@@ -2,7 +2,45 @@
 #include <stdlib.h>
 
 int main(void) {
-    puts("!!!Hello World!!!");
+    char q1 = 'R';
+    char q2 = 'G';
+    char q3 = 'B';
+    char q4 = 'Y';
+
+    puts("【色当てゲーム】");
+    puts("ゲームをはじめてください。");
+    puts("コンピュータが問題を出しました。");
+    puts("予想を入力してください。");
+
+    // コンソールからの取得
+    char t1 = getchar();
+    char t2 = getchar();
+    char t3 = getchar();
+    char t4 = getchar();
+
+    // コンソールへの表示
+    putchar(t1);
+    putchar(t2);
+    putchar(t3);
+    putchar(t4);
+
+    int matched = 0;
+    //条件分岐
+    if(q1 == t1) {
+        matched ++;
+    }
+    if(q2 == t2) {
+        matched ++;
+    }
+    if(q3 == t3) {
+        matched ++;
+    }
+    if(q4 == t4) {
+        matched ++;
+    }
+
+    puts("結果");
+    printf("%d コ合っています。 \n", matched);
     // 記号定数→enum
     return EXIT_SUCCESS;
 }
